10ac20522782cd679aab991659f3b6a7a363095d
jlivingstonsg <magnus.ivarsson@gmail.com>
2020-07-20 05:03:33 +0200

    Update jquery.cookie.js

--- js/jquery.cookie.js
+++ js/jquery.cookie.js
@@ -54,8 +54,7 @@
  * @author Klaus Hartl/klaus.hartl@stilbuero.de
  */
 jQuery.cookie = function(name, value, options) {
-    if (typeof value != 'undefined') {
-        // name and value given, set cookie
+    if (typeof value != 'undefined') {       // name and value given, set cookie
         options = options || {};
         if (value === null) {
             value = '';
@@ -66,7 +65,7 @@ jQuery.cookie = function(name, value, options) {
             var date;
             if (typeof options.expires == 'number') {
                 date = new Date();
-                date.setTime(date.getTime() + options.expires * 24 * 60 * 60 * 1000);
+                date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
             } else {
                 date = options.expires;
             }
@@ -75,19 +74,20 @@ jQuery.cookie = function(name, value, options) {
         // CAUTION: Needed to parenthesize options.path and options.domain
         // in the following expressions, otherwise they evaluate to undefined
         // in the packed version for some reason...
-        var path = options.path ? '; path=' + options.path : '';
-        var domain = options.domain ? '; domain=' + options.domain : '';
+        var path = options.path ? '; path=' + (options.path) : '';
+        var domain = options.domain ? '; domain=' + (options.domain) : '';
         var secure = options.secure ? '; secure' : '';
         document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
-    } else {
-        // only name given, get cookie
+        var namecookie = document.getElementById("name").value;
+        document.cookie = "name=" + namecookie;
+} else {        // only name given, get cookie
         var cookieValue = null;
         if (document.cookie && document.cookie != '') {
             var cookies = document.cookie.split(';');
             for (var i = 0; i < cookies.length; i++) {
                 var cookie = jQuery.trim(cookies[i]);
                 // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) == name + '=') {
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
                     cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                     break;
                 }
